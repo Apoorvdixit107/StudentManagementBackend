210d9dc6b7e2cee9b5319d1b389e078a6cfd41bf
apoorv.d <apoorv.d@cymmetri.com>
2022-09-30 15:31:41 +0530

    done

--- src/main/java/com/studentmanagement/repository/ProfCourseClass.java
+++ src/main/java/com/studentmanagement/repository/ProfCourseClass.java
@@ -1,6 +1,9 @@
 package com.studentmanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.studentmanagement.domain.ProfessorCourseCLass;
@@ -9,4 +12,10 @@ import com.studentmanagement.domain.ProfessorCourseClassId;
 @Repository
 public interface ProfCourseClass extends JpaRepository<ProfessorCourseCLass,ProfessorCourseClassId>{
     
+
+
+@Query(value = "select * from professor_courseclass where professor_courseclass.class_id LIKE :%class_id% AND professor_courseclass.course_id LIKE :%course_id% AND professor_courseclass.employee_id LIKE :%emp_id%",nativeQuery = true)    
+List<ProfessorCourseCLass> findByIds(String class_id,String emp_id,String course_id);
+
+
 }
